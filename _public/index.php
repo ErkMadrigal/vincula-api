8275144e3f006a96eb64263283244905d5f5652a
Erk <erick.madfl@gmail.com>
2026-04-25 19:38:36 -0600

    public deploy

--- _public/index.php
+++ _public/index.php
@@ -3,6 +3,7 @@
 use CodeIgniter\Boot;
 use Config\Paths;
 
+
 /*
  *---------------------------------------------------------------
  * CHECK PHP VERSION
@@ -48,7 +49,9 @@ if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
 
 // LOAD OUR PATHS CONFIG FILE
 // This is the line that might need to be changed, depending on your folder structure.
-require FCPATH . '../app/Config/Paths.php';
+// require FCPATH . '../app/Config/Paths.php';
+require '/home2/erickmad/vincula-api/app/Config/Paths.php';
+
 // ^^^ Change this line if you move your application folder
 
 $paths = new Paths();
